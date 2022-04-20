bf7cb1e92b601d599ad959c2aa065969dfaee414
Sereda Ihor <seredaihor@ukr.net>
2022-04-20 17:32:00 +0300

    add home work 10.3-10.4

--- home_work/home_work_10.py
+++ home_work/home_work_10.py
@@ -23,3 +23,22 @@ with open(file_name, encoding="utf8") as file_object:
 for line in lines:
     edit_line = line.replace('Python','Java').rstrip()
     print(edit_line)
+
+print('\t')
+
+def get_user_name():
+    """Get user_name"""
+    user_name = input('Enter your name: ')
+    if not user_name:
+        user_name = get_user_name()
+    print(f"Hello {user_name}")
+    return user_name
+
+def write_user_info(user, ref='text_files/guest_book.txt'):
+    """Write user info"""
+    with open(ref, 'a', encoding="utf8") as user_object:
+        user_object.write(f"Added info about user: {user} \n")
+        print('Record was added!')
+
+new_user = get_user_name()
+write_user_info(new_user)
