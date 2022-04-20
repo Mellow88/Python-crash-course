1abbe2321aa6a8d28bd2fa5fc6d46cda838b90d1
Sereda Ihor <seredaihor@ukr.net>
2022-04-20 18:32:04 +0300

    add info about exceptions

--- unit/unit_10.py
+++ unit/unit_10.py
@@ -62,3 +62,41 @@ print('\t')
 # NOTE: Додавання у файл нової інформації
 with open(file_name, 'a', encoding="utf8") as file_object:
     file_object.write('I also love C++')
+
+# NOTE: Винятки
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print('You can not divide by zero')
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    else:
+        print(answer)
+
+print('\t')
+
+file_name = 'text_files/alice.txt'
+
+try:
+    with open(file_name, encoding="utf8") as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {file_name} does not exist")
+else:
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {file_name} has about {num_words} words.")
+    print(words)
