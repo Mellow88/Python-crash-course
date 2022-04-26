eb41bdb16117e8b3172ed54eba62c5b949e2dc0a
Sereda Ihor <seredaihor@ukr.net>
2022-04-26 13:22:53 +0300

    add main module for game

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,35 @@
+"""Alien invasion"""
+
+import sys
+import pygame
+from settings import Settings
+
+class AlienIvasion:
+    """Загальний клас, що керує ресурсами та поведінкою гри."""
+
+    def __init__(self):
+        """Ініціалізація гри, створенння ресурсів гри"""
+        pygame.init()
+
+        self.settings  = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Початок головного циклу гри"""
+        while True:
+            # NOTE: Слідкування за подіями миші та клавіатури
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+            # NOTE: Останній намальований екран
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # NOTE: Створення екземпляру гри та її запуск
+    ai = AlienIvasion()
+    ai.run_game()
