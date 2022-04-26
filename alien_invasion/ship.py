8fcb6375807612c1089e92c3db0cad2afd37eaaa
Sereda Ihor <seredaihor@ukr.net>
2022-04-26 14:30:30 +0300

    update ai

---
+++ alien_invasion/ship.py
@@ -0,0 +1,26 @@
+"""Class Ship"""
+
+import pygame
+
+class Ship():
+    """Клас для керування космічним кораблем."""
+
+    def __init__(self, ai_game):
+        """Ініціалізація корабля та його початкового положення"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # NOTE: Завантаження зображення корабля
+        image = pygame.image.load('images/space_ship.png')
+        image = pygame.transform.scale(image, (64, 128))
+
+        self.image = image
+        self.rect = self.image.get_rect()
+
+
+        # NOTE: Створення кожного нового корабля внизу по центру екрану
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Намалювати корабель у його поточному розташуванні"""
+        self.screen.blit(self.image, self.rect)
