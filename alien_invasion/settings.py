6088185b12b3d4a7ee5347a66dd3727ebe0fedfe
Sereda Ihor <seredaihor@ukr.net>
2022-04-26 13:22:25 +0300

    add class settings for game

---
+++ alien_invasion/settings.py
@@ -0,0 +1,11 @@
+"""Settings for game"""
+
+class Settings():
+    """Class for game settings."""
+
+    def __init__(self):
+        """Ініціалізація параметрів гри"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
