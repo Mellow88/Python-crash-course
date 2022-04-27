6a75fbf8fca5f459e352f3ce01bd71ee3455931a
Sereda Ihor <seredaihor@ukr.net>
2022-04-27 13:30:20 +0300

    add settings for ship add new metodh 'update'

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -9,6 +9,7 @@ class Ship():
         """Ініціалізація корабля та його початкового положення"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # NOTE: Завантаження зображення корабля
         image = pygame.image.load('images/space_ship.png')
@@ -20,6 +21,31 @@ class Ship():
 
         # NOTE: Створення кожного нового корабля внизу по центру екрану
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # NOTE: Збереження десяткового значення позиції корабля по горизонталі
+        self.x = float(self.rect.x )
+
+        # NOTE: Індикатори руху
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """
+        Оновлення поточної позиції корабля на основі
+        індикатора руху
+        """
+        # if self.moving_right:
+        #     self.x += self.settings.ship_speed
+        # if self.moving_left:
+        #     self.x -= self.settings.ship_speed
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """Намалювати корабель у його поточному розташуванні"""
