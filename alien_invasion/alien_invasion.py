52b1a711866c1249a13e34eeee890646cffeba92
Sereda Ihor <seredaihor@ukr.net>
2022-04-27 13:31:27 +0300

    update metodh '_check_events'

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,11 +30,34 @@ class AlienIvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # NOTE: Перевіряємо чи натиснута клавіша
+            # NOTE: Змінюємо атрибут moving_right корабля
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагування на натискання клавіш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Реагування, коли клавіша не натиснута"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def run_game(self):
         """Початок головного циклу гри"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 
