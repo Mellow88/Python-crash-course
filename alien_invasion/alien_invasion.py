ed0642e49d799b2ccfc06adb4c4f63e45e40dd95
Sereda Ihor <seredaihor@ukr.net>
2022-05-03 11:34:44 +0300

    update main file

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -86,17 +86,54 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _create_fleet(self):
-        """Створення флоту прибульців"""
+        """Створення флоту кораблів прибульців"""
         alien = Alien(self)
-        alien_width =  alien.rect.width
+        alien_width, alien_height =  alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_alien_x = available_space_x // (2 * alien_width)
 
-        for alien_num in range(number_alien_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_num
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # NOTE: Визначення кількості рядів прибульців,
+        # NOTE: що поміщається на екрані
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                            (3 * alien_height) - ship_height)
+        numbers_row = available_space_y // (2 * alien_height)
+
+        for row_num in range(numbers_row):
+            for alien_num in range(number_alien_x):
+                self._create_alien(alien_num, row_num)
+
+    def _create_alien(self, alien_num, row_num):
+        """Створення корабля прибульця"""
+        alien = Alien(self)
+        alien_width, alien_height =  alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_num
+        alien.y = alien.rect.height + 2 * alien.rect.height * row_num
+        alien.rect.x = alien.x
+        alien.rect.y = alien.y
+        self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """
+        Реагує відповідно до того, чи досяг котрийсь
+        із прибульців краю екрана.
+        """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Зміна напрямку руху флоту"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+    def _update_aliens(self):
+        """"Оновлення позицій космічних кораблів прибульців"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def run_game(self):
         """Початок головного циклу гри"""
@@ -104,6 +141,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
