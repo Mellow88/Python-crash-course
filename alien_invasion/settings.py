f8c7aaab3283464e787ee38af9e0ffd2266a33d9
Sereda Ihor <seredaihor@ukr.net>
2022-05-15 19:23:49 +0300

    add new method for class

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,6 +9,7 @@ class Settings():
         """Ініціалізація параметрів гри"""
         # Game settings
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
         # Screen settings
@@ -51,3 +52,6 @@ class Settings():
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
